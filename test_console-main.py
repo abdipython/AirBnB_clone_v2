05c0a8544051760917905ecd35223b278e0c9203
Abdulrahman Saumu <abdulrahmansaumu@gmail.com>
2024-04-14 09:34:25 -0700

    done with tasks

---
+++ test_console-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import inspect
+import io
+import sys
+import cmd
+import shutil
+
+"""
+Cleanup file storage
+"""
+import os
+file_path = "file.json"
+if not os.path.exists(file_path):
+    try:
+        from models.engine.file_storage import FileStorage
+        file_path = FileStorage._FileStorage__file_path
+    except:
